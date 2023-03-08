725e87543eb1859969b8c1d0031499dcfb64ff4e
wookimodo <rudghk1437@naver.com>
2023-03-08 20:25:29 +0900

    자바의 클래스 실습

---
+++ Chapter4/src/ch03/StringBuilderTest.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class StringBuilderTest {
+	
+	public static void main(String[] args) {
+		String java = new String("java");
+		String android = new String("android");
+		
+		StringBuilder buffer = new StringBuilder(java);
+		System.out.println(System.identityHashCode(buffer));
+		buffer.append("android"); // append로 추가
+		System.out.println(System.identityHashCode(buffer));
+		
+		java = buffer.toString();
+	}
+
+}
---
+++ Chapter4/src/ch03/TextBlockTest.java
@@ -0,0 +1,29 @@
+package ch03;
+
+public class TextBlockTest {
+	public static void main(String[] args) {
+		
+		String strBlock = """
+				This 
+				is 
+				text
+				block
+				test.""";
+		System.out.println(strBlock);
+		
+		System.out.println(getBlockOfHtml());
+		
+	}
+	
+	public static String getBlockOfHtml() {
+		    return """
+		            <html>
+
+		                <body>
+		                    <span>example text</span>
+		                </body>
+		            </html>""";
+		
+	}
+
+}
