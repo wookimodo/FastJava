63a6ca5e922acc82f580046601ce3dde4f91a05a
DESKTOP-0A9J2K9\KIM <rudghk1437@naver.com>
2023-03-10 00:45:56 +0900

    HashSet, HashMap 학습

--- Chapter5/src/ch10/MemberArrayList.java
+++ Chapter5/src/ch10/MemberArrayList.java
@@ -1,6 +1,7 @@
 package ch10;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
 
@@ -20,15 +21,26 @@ public class MemberArrayList {
 	
 	public boolean removeMember(int memberId){  // 멤버 아이디를 매개변수로, 삭제 여부를 반환
 		
-		for(int i =0; i<arrayList.size(); i++){ // 해당 아이디를 가진 멤버를 ArrayList에서 찾음
-			Member member = arrayList.get(i);
+//		for(int i =0; i<arrayList.size(); i++){ // 해당 아이디를 가진 멤버를 ArrayList에서 찾음
+//			Member member = arrayList.get(i);
+//			int tempId = member.getMemberId();
+//			if(tempId == memberId){            // 멤버아이디가 매개변수와 일치하면 
+//				arrayList.remove(i);           // 해당 멤버를 삭제
+//				return true;                   // true 반환
+//			}
+//		}
+		
+		// iterator 사용하기
+		Iterator<Member> ir = arrayList.iterator();
+		while(ir.hasNext()) {
+			Member member = ir.next();
 			int tempId = member.getMemberId();
 			if(tempId == memberId){            // 멤버아이디가 매개변수와 일치하면 
-				arrayList.remove(i);           // 해당 멤버를 삭제
+				arrayList.remove(member);           // 해당 멤버를 삭제
 				return true;                   // true 반환
 			}
 		}
-		
+
 		System.out.println(memberId + "가 존재하지 않습니다");  //for 가 끝날때 까지 return 이 안된경우
 		return false;                   
 	}
